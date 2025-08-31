6ac6172f1bed58aac421f49e75a27c58ed6ced8c
xlrzwtvq <kumar.amit@icloud.com>
2025-08-31 21:26:58 +0530

    Navigation bar hamburger for mobile not working - reversing code for now

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('nav-links');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
